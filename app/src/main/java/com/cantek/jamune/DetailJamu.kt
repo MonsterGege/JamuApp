7611540057bb77a3b3f606ac351f0c63d73599cc
MonsterGege <georgeonsent10@mgmail.com>
2021-04-13 01:53:17 +0700

    oke

---
+++ app/src/main/java/com/cantek/jamune/DetailJamu.kt
@@ -0,0 +1,11 @@
+package com.cantek.jamune
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailJamu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_jamu)
+    }
+}
