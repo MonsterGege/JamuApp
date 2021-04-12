1879f3213d587329db68fad36bfcde6f22c5556b
MonsterGege <georgeonsent10@mgmail.com>
2021-04-12 14:05:50 +0700

    first commit

---
+++ app/src/main/java/com/cantek/jamune/ListJamu.kt
@@ -0,0 +1,11 @@
+package com.cantek.jamune
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListJamu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_jamu)
+    }
+}
