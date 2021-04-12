a3d96c8de1d8a4ee0a0143b5f80153eeac7bd32a
MonsterGege <georgeonsent10@mgmail.com>
2021-04-12 18:45:03 +0700

    Base MainActivity

--- app/src/main/java/com/cantek/jamune/MainActivity.kt
+++ app/src/main/java/com/cantek/jamune/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.cantek.jamune
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    private lateinit var title: TextView
+    private lateinit var ket: TextView
+    private lateinit var img: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        title = findViewById(R.id.jdl)
+        ket = findViewById(R.id.keterangan)
+
     }
 }
