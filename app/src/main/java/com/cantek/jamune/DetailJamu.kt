545304d62c9401c0ebfcae13560051b22a8446f8
MonsterGege <georgeonsent10@mgmail.com>
2021-04-13 01:53:31 +0700

    oke

--- app/src/main/java/com/cantek/jamune/DetailJamu.kt
+++ app/src/main/java/com/cantek/jamune/DetailJamu.kt
@@ -1,9 +1,10 @@
 package com.cantek.jamune
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class DetailJamu : AppCompatActivity() {
+class DetailJamu : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_jamu)
--- app/src/main/java/com/cantek/jamune/Notifications.kt
+++ app/src/main/java/com/cantek/jamune/Notifications.kt
@@ -7,5 +7,6 @@ class Notifications : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notifications)
+
     }
 }
