b4e9c0788fee1096c4685e3676e33c9d457c2b63
taniarachel <672018370@student.uksw.edu>
2021-04-13 15:02:50 +0700

    fragment

---
+++ app/src/main/java/com/cantek/jamune/detailKategori.kt
@@ -0,0 +1,11 @@
+package com.cantek.jamune
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detailKategori : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_kategori)
+    }
+}
