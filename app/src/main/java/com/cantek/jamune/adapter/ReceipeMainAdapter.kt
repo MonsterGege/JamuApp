aa1225b4f4a6076605229901280275ad8c0dee9a
MonsterGege <georgeonsent10@mgmail.com>
2021-04-14 21:12:56 +0700

    Tinggal Glide

--- app/src/main/java/com/cantek/jamune/adapter/ReceipeMainAdapter.kt
+++ app/src/main/java/com/cantek/jamune/adapter/ReceipeMainAdapter.kt
@@ -6,17 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.cantek.jamune.R
 import com.cantek.jamune.model.Receipe
 import kotlinx.android.synthetic.main.listjamu.view.*
 
-class ReceipeMainAdapter(private val recipe: List<Receipe>): RecyclerView.Adapter<ReceipeMainAdapter.MyViewHoleder>() {
-
+class ReceipeMainAdapter(context: Context,private val recipe: List<Receipe>): RecyclerView.Adapter<ReceipeMainAdapter.MyViewHoleder>() {
+    var konteks: Context
+    init {
+        konteks = context
+    }
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ReceipeMainAdapter.MyViewHoleder {
         return MyViewHoleder(LayoutInflater.from(parent.context).inflate(R.layout.listjamu,parent,false))
+
     }
 
     override fun getItemCount(): Int = recipe.size
@@ -32,7 +37,6 @@ class ReceipeMainAdapter(private val recipe: List<Receipe>): RecyclerView.Adapte
 
         fun bindrecipe(recipes: Receipe){
             //image pake glide gk sih ???
-
             jdl.text = recipes.judul
             keterangan.text = recipes.deskripsi
         }
