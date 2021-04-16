9210e08210c0529cebba0057a72c0316902743ec
taniarachel <672018370@student.uksw.edu>
2021-04-16 14:54:15 +0700

    search

--- app/src/main/java/com/cantek/jamune/MainActivity.kt
+++ app/src/main/java/com/cantek/jamune/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.cantek.jamune
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.cantek.jamune.adapter.PageAdapter
@@ -16,6 +18,21 @@ class MainActivity : AppCompatActivity() {
         val tabLayout: TabLayout = findViewById(R.id.tabs)
         val viewPager: ViewPager = findViewById(R.id.main_view_pager)
         val search: SearchView = findViewById(R.id.search)
+
+
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val i = Intent(this@MainActivity,Search::class.java)
+                i.putExtra("key-query",query)
+                startActivity(i)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
 
         viewPager.adapter = PageAdapter(this, supportFragmentManager)
         tabLayout.setupWithViewPager(viewPager)
--- app/src/main/java/com/cantek/jamune/Search.kt
+++ app/src/main/java/com/cantek/jamune/Search.kt
@@ -1,41 +1,33 @@
 package com.cantek.jamune
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cantek.jamune.adapter.CategoryAdapter
-import com.cantek.jamune.adapter.ReceipeMainAdapter
 import com.cantek.jamune.model.Receipe
 import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.fragment_list.*
 
-class DetailKategori : AppCompatActivity() {
+class Search : AppCompatActivity() {
 
-    companion object{
-        const val EXTRA_DATA = ""
-    }
-    private lateinit var rvJamu : RecyclerView
-
-
+    private lateinit var rvSearch : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_kategori)
+        setContentView(R.layout.activity_search)
 
+        rvSearch = findViewById(R.id.recyclersearch)
+        val query = intent.getStringExtra("key-query")
+        val hasil : TextView = findViewById(R.id.hasil)
+        val empty : TextView = findViewById(R.id.tidakada)
+        empty.text = ""
+        hasil.text = query
 
-        rvJamu = findViewById(R.id.recycler)
-        val get = intent.getStringExtra(EXTRA_DATA)
-        //supportActionBar?.title=get
-
-
-        var CatRecipe: MutableList<Receipe> = mutableListOf()
+        val SearchRecipe: MutableList<Receipe> = mutableListOf()
         val ref: DatabaseReference = FirebaseDatabase.getInstance("https://jamune-67b20-default-rtdb.firebaseio.com/").getReference("resep")
 
-        ref.addValueEventListener(object :ValueEventListener{
+        ref.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(data: DataSnapshot) {
                 if (data.exists()){
                     for (item in data.children){
@@ -47,8 +39,8 @@ class DetailKategori : AppCompatActivity() {
                         for (i in item.child("bahan").children) {
                             bahan.add(i.value.toString())
                         }
-                        if (get in bahan || get in khasiat) {
-                            CatRecipe.add(
+                        if (query in bahan || query in khasiat || query == item.child("judul").value.toString()  ) {
+                            SearchRecipe.add(
                                 Receipe(
                                     key = item.key.toString(),
                                     judul = item.child("judul").value.toString(),
@@ -64,12 +56,13 @@ class DetailKategori : AppCompatActivity() {
 
                     }
 
-                    rvJamu.layoutManager = LinearLayoutManager(this@DetailKategori)
-                    val categoryAdapter = CategoryAdapter(this@DetailKategori,CatRecipe)
-                    rvJamu.adapter = categoryAdapter
+                    rvSearch.layoutManager = LinearLayoutManager(this@Search)
+                    val categoryAdapter = CategoryAdapter(this@Search,SearchRecipe)
+                    rvSearch.adapter = categoryAdapter
 
-                        }
-                    }
+                }
+
+            }
 
             override fun onCancelled(error: DatabaseError) {
                 Log.w("error=","Error bund")
